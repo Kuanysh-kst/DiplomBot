c985f82875cc48f643ff189865a2a2cfc19ab864
Kuanysh <kuanysh4646@gmail.com>
2023-02-18 16:59:16 +0600

    сделал простой слайдер

--- src/main/java/kz/kuanysh/bot/chain/chains/ShowResultChain.java
+++ src/main/java/kz/kuanysh/bot/chain/chains/ShowResultChain.java
@@ -7,6 +7,7 @@ import kz.kuanysh.bot.service.UserService;
 import kz.kuanysh.bot.state.Dialog;
 import kz.kuanysh.bot.state.UserActivity;
 import kz.kuanysh.bot.state.states.ShowResultState;
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -14,7 +15,10 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class ShowResultChain extends DialogChain {
+
+    private int index;
 
     public ShowResultChain(DialogChain nextChain) {
         super(nextChain);
@@ -22,12 +26,17 @@ public class ShowResultChain extends DialogChain {
 
     @Override
     protected void doProcess(Message message, Dialog state, String command, UserService userService, SendBotMessageServiceImp execute) {
+        int val = this.index;
+
         if (command.equals("/result")) {
+            this.index = 0;
             userService.saveUserParameters(message, state);
             List<User> list = userService.findByChoiceAndCategory(message);
             Optional<User> userOp = list.stream().findFirst();
+
             if (userOp.isPresent()) {
                 User user = userOp.get();
+
                 var response = user.getFile();
                 InputFile inputFile = new InputFile(response);
                 SendPhoto sendPhoto = SendPhoto.builder()
@@ -38,12 +47,45 @@ public class ShowResultChain extends DialogChain {
                 execute.sendPhoto(sendPhoto);
             }
 
+        } else if (command.equals("/next") ) {
+            val++;
+            List<User> userList = userService.findByChoiceAndCategory(message);
+            if (val < userList.size() && val >= 0) {
+                this.index++;
+                state.setUserIndex(+1);
+
+                User user = userList.get(index);
+                var response = user.getFile();
+                InputFile inputFile = new InputFile(response);
+                SendPhoto sendPhoto = SendPhoto.builder()
+                        .chatId(message.getChatId().toString())
+                        .photo(inputFile)
+                        .caption(user.getFirstName())
+                        .build();
+                execute.sendPhoto(sendPhoto);
+            }
+
+        } else if (command.equals("/prev")) {
+            List<User> userList = userService.findByChoiceAndCategory(message);
+            val--;
+            if (val < userList.size() && val >= 0) {
+                --this.index;
+                User user = userList.get(index);
+                var response = user.getFile();
+                InputFile inputFile = new InputFile(response);
+                SendPhoto sendPhoto = SendPhoto.builder()
+                        .chatId(message.getChatId().toString())
+                        .photo(inputFile)
+                        .caption(user.getFirstName())
+                        .build();
+                execute.sendPhoto(sendPhoto);
+            }
         } else if (command.equals("/back")) {
             state.backDialogState();
             userService.saveDialog(message, state);
             state.backDialogState();
 
-             state.executeMessage(message, command,execute);
+            state.executeMessage(message, command, execute);
         }
     }
 
