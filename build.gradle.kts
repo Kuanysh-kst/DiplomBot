14d15a3f63b11f92142b5cef0767688a87407bef
Kuanysh Aimurzinov <kuanysh4646@gmail.com>
2023-06-20 12:42:39 +0600

    added fix
    Signed-off-by: Kuanysh Aimurzinov <kuanysh4646@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ configurations {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter:3.1.0")
+    implementation("org.springframework.boot:spring-boot-starter" )
     implementation("org.telegram:telegrambots:6.5.0")
     implementation("ch.qos.logback:logback-classic:1.2.11")
     implementation("org.postgresql:postgresql:42.5.4")
